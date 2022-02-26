0ac8d66356f216eba25c2bb176607f309984ae0a
J-467 <jotweneboakodua@gmail.com>
2022-02-26 14:50:16 -0500

    Simple flashcards lab done

--- Flashcard/ViewController.swift
+++ Flashcard/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flashText1: UILabel!
+    @IBOutlet weak var randombox: UIView!
+    @IBOutlet weak var randomText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        flashText1.text = "Usain Bolt"
+        randombox.backgroundColor = .green
+    }
+    
 }
-
