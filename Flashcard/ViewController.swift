030d75bfb6569bf7a244e42bc735d48cbc8c0d92
J-467 <jotweneboakodua@gmail.com>
2022-02-26 13:33:58 -0500

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Julian Tweneboa Kodua on 2/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
